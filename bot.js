d0236be6854e92c4e3d5ed98641580aaf0eee2f1
Mu6la8 , David <44671606+CrazyVlose@users.noreply.github.com>
2018-11-30 01:16:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const bot = new Discord.Client();
+const token = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as  * [ " BC " ] servers! [ " ${client.guilds.size} " ]`);
   client.user.setStatus('dnd');
   });
-  
-  client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-  
 
 client.on('guildMemberRemove', member => {
-
+ console.log("he's ready now");
      member.createDM().then(function (channel1) {
           var test = setInterval (function (){
- channel1.send(`**
+ channel1.send(`
+**
                           السلام عليكم
                   عسا ما ازعجتك يا عمري
-      "Orexn"    ننتظر سموك الراقي بسيرفرنا  
+      "Orexn"    ننتظر سموك الراقي بسيرفرنا
                     تنور السيرفر حياك الله
 الرابط : https://discord.gg/YzTPeS7
-**`);
+**
+`);
 
           },1000*60*5);
 
@@ -30,16 +28,18 @@ client.on('guildMemberRemove', member => {
 });
 
 client.on('guildMemberAdd', member => {
-
+ console.log("he's ready now");
      member.createDM().then(function (channel1) {
           var test = setInterval (function (){
- channel1.send(`**
+ channel1.send(`
+**
                           السلام عليكم
                   عسا ما ازعجتك يا عمري
-      "Orexn"    ننتظر سموك الراقي بسيرفرنا  
+      "Orexn"    ننتظر سموك الراقي بسيرفرنا
                     تنور السيرفر حياك الله
 الرابط : https://discord.gg/YzTPeS7
-**`);
+**
+`);
 
           },1000*60*5);
 
@@ -49,4 +49,6 @@ client.on('guildMemberAdd', member => {
 
 
 // Log our bot in
-+client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
+token.login(process.env.OP);
+bot.login(process.env.SH);
