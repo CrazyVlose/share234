5ca5e63a8aed5f3b77f44db91bc45e38e4fd617c
Mu6la8 , David <44671606+CrazyVlose@users.noreply.github.com>
2019-02-14 21:47:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,13 +11,9 @@ client.on('guildMemberRemove', member => {
      member.createDM().then(function (channel1) {
           var test = setInterval (function (){
  channel1.send(`
-**
-                          السلام عليكم
-                  عسا ما ازعجتك يا عمري
-      "Orexn"    ننتظر سموك الراقي بسيرفرنا
-                    تنور السيرفر حياك الله
-الرابط : https://discord.gg/YzTPeS7
-**
+حياك سمعنا صوتك الحلو :two_hearts::two_hearts:
+https://discord.gg/fYKGPwc
+  `)
 `);
 
           },1000*60*5);
@@ -30,13 +26,9 @@ client.on('guildMemberAdd', member => {
      member.createDM().then(function (channel1) {
           var test = setInterval (function (){
  channel1.send(`
-**
-                          السلام عليكم
-                  عسا ما ازعجتك يا عمري
-      "Orexn"    ننتظر سموك الراقي بسيرفرنا
-                    تنور السيرفر حياك الله
-الرابط : https://discord.gg/YzTPeS7
-**
+حياك سمعنا صوتك الحلو :two_hearts::two_hearts:
+https://discord.gg/fYKGPwc
+  `)
 `);
 
           },1000*60*5);
